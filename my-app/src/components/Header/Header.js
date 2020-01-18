e0e7578f2ea57c46e17938510c5a047fb3588036
rumaniac <boseinsideyou@gmail.com>
2020-01-18 22:12:10 +0530

    react-router-dom added,Header & Main folder aded with js files

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Our header started</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ my-app/src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+    return (
+        <main>
+            <h1>THIS is the main component </h1>
+        </main>
+    );
+}
+
+export default Main;
